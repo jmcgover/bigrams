beb70f72b234935f003a43ee6ae7ac945c1b3672
Jeff McGovern <jmcgover@calpoly.edu>
2014-12-17 23:25:32 -0800

    not sure what I did to this...

--- Updater.java
+++ Updater.java
@@ -1,19 +1,32 @@
 import twitter4j.*;
+import twitter4j.conf.*;
+import java.io.UnsupportedEncodingException;
 
 public class Updater{
    private TwitterFactory tf;
    private Twitter twitter;
    public Updater(){
-      this.tf = new TwitterFactory();
+      ConfigurationBuilder cb = new ConfigurationBuilder();
+      cb.setDebugEnabled(true)
+         .setOAuthConsumerKey("Lj0mBFp3cq8ZM5zP0OdPDZuGn")
+         .setOAuthConsumerSecret("Pz2fY8VCqqkAovDaJEZIANYCDQa5AAN6NiPo2eKaqCGvysn1ek")
+         .setOAuthAccessToken("2902287073-gFvwqMAvUNWqEcKPIu2KXX59b4p61bDxNivgpiQ")
+         .setOAuthAccessTokenSecret("XLeiKHNwVpn8Z42WpLC8fm4O2N1Z5Ps0jZQyWqhDAtf2Z");
+      this.tf = new TwitterFactory(cb.build());
       this.twitter = tf.getInstance();
-
    }
    // Update Status
    public boolean updateStatus(String text){
       try {
-         StatusUpdate latestStatus = new StatusUpdate(text);
+         byte[] utf8TextBytes = text.getBytes("UTF8");
+         String utf8TextString = new String(utf8TextBytes, "UTF8");
+
+         StatusUpdate latestStatus = new StatusUpdate(utf8TextString);
          Status status = this.twitter.updateStatus(latestStatus);
       } catch (TwitterException e) {
+         e.printStackTrace();
+         return false;
+      } catch (UnsupportedEncodingException e) {
          e.printStackTrace();
          return false;
       }
